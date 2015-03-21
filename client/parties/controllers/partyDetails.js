074c22ea9a31ee0fae07e6624f13b17cf345d616
perjo927 <perjo927@student.liu.se>
2015-03-21 10:30:11 +0000

    Ready for deploy

--- client/parties/controllers/partyDetails.js
+++ client/parties/controllers/partyDetails.js
@@ -1,6 +1,7 @@
 
 angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor', function($scope, $stateParams, $meteor){
-    $scope.party = $meteor.object(Parties, $stateParams.partyId, false);
+    $scope.party = $meteor.object(Parties, $stateParams.partyId).subscribe('parties');
+    $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
 
     $scope.save = function() {
         $scope.party.save().then(function(numberOfDocs){
--- model/parties.js
+++ model/parties.js
@@ -17,3 +17,4 @@ Parties.allow({
     return true;
   }
 });
+
