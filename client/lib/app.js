4b9c2963493705f60186ffc0ff7f3ba3db346af2
Per Jonsson <perjo927@student.liu.se>
2015-03-29 20:11:06 +0200

    Added material desgin

--- client/lib/app.js
+++ client/lib/app.js
@@ -2,9 +2,9 @@ angular.module('socially',[
     'angular-meteor',
     'ui.router',
     'angularUtils.directives.dirPagination',
-    'uiGmapgoogle-maps',
-])
-  
+    'uiGmapgoogle-maps'
+]);
+
 // TODO: DOesn't work when deploying on meteor
 //    .config(function($interpolateProvider) {
 //    $interpolateProvider.startSymbol('[[');
@@ -16,8 +16,8 @@ function onReady() {
     angular.bootstrap(document, ['socially']);
 }
 
-if (Meteor.isCordova)
+if (Meteor.isCordova) {
     angular.element(document).on("deviceready", onReady);
-else
+} else {
     angular.element(document).ready(onReady);
-
+}
